da002856617a9a54b3d6e03e5ff819b5d86c3fc0
Carlos <gohercarlos@gmail.com>
2021-11-02 02:10:43 -0500

    Cambio

--- fromvalidation/lib/main.dart
+++ fromvalidation/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        initialRoute: 'home',
+        initialRoute: 'login',
         routes: {
           'login': (BuildContext context) => LoginPage(),
           'home': (BuildContext context) => HomePage(),
